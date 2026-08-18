2b9d30a2967db12a6e16b88eb076c17d302e7bd1
Said Hadji <saidhadji.dev@gmail.com>
2026-08-18 16:21:32 +0100

    feat: add fixed-top CSS utility

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Home from "./intro/Home";
 import Navbar from "./navbar/Navbar";
 
 function App() {
   return (
-    <div className="fill-screen bg-sablis-graphite">
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
