e6729a29799e068d75d1c2a37e1292d1fdfeb9cf
Said Hadji <saidhadji.dev@gmail.com>
2026-08-18 20:09:54 +0100

    feat: add below-parent CSS utility

--- src/navbar/menu/MenuContainer.jsx
+++ src/navbar/menu/MenuContainer.jsx
@@ -1,7 +1,9 @@
 export default function MenuContainer() {
   return (
-    <div
-      className={`absolute top-full left-0 z-50 w-80 h-screen bg-sablis-surface border border-sablis-border`}
-    ></div>
+    <div className={`below-parent`}>
+      <div
+        className={`w-80 h-screen bg-sablis-surface border border-sablis-border`}
+      ></div>
+    </div>
   );
 }
