4795d94f39355b23cdc61295c3ba27b0812327af
Said Hadji <saidhadji.dev@gmail.com>
2026-08-18 16:34:22 +0100

    feat: create menu container

--- src/intro/Home.jsx
+++ src/intro/Home.jsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return console.log("Home");
+  return (
+    <div>
+      <div className={`fill-screen bg-sablis-graphite`}></div>
+    </div>
+  );
 }
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,12 +3,14 @@ import MenuButton from "./MenuButton";
 
 export default function Navbar() {
   return (
-    <div>
-      <div className={`w-full flex-between px-10 py-7`}>
+    <div className={`fixed-top`}>
+      <div className={`w-full bg-sablis-graphite flex-between px-10 py-7`}>
         <MenuButton />
         <h1 className={`text-sablis-primary text-3xl font-display select-none`}>SABLIS</h1>
         <AuthButton />
       </div>
+
+      <div className={`absolute top-full left-0 z-50 w-80 h-screen bg-sablis-surface border border-sablis-border`}></div>
     </div>
   );
 }
