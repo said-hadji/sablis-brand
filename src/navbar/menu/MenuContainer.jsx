2b1f1b10cd14f4f44635e7d00ed13c49285ace98
Said Hadji <saidhadji.dev@gmail.com>
2026-08-18 20:26:18 +0100

    feat: control menu visibility with isMenu state

--- src/navbar/menu/MenuContainer.jsx
+++ src/navbar/menu/MenuContainer.jsx
@@ -1,6 +1,10 @@
+import useApp from "../../hooks/useApp";
+
 export default function MenuContainer() {
+  const { isMenu } = useApp();
+
   return (
-    <div className={`below-parent`}>
+    <div className={["below-parent duration-300", isMenu ? "translate-x-0" : "-translate-x-full"].join(" ")}>
       <div
         className={`w-80 h-screen bg-sablis-surface border border-sablis-border`}
       ></div>
